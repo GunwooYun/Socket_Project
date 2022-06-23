6bc2bfa3f8d9714269aca5fe266821e2df8b5cfb
GunwooYun <gunwoo87@gmail.com>
2022-06-23 20:48:37 +0900

    connect wifi, send msg to sever

--- client/Client.ino
+++ client/Client.ino
@@ -1,13 +1,17 @@
-/*
- WiFiEsp example: WebClient
-
- This sketch connects to google website using an ESP8266 module to
- perform a simple web search.
-
- For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
-*/
-
 #include "WiFiEsp.h"
+#include <MsTimer2.h>
+#include <DFRobot_DHT11.h>
+
+DFRobot_DHT11 DHT;
+#define DHT11_PIN 8
+#define LED_PIN 2
+#define FLAME_PIN A5
+#define CDS_PIN A0
+
+int cdsValue;
+int flameValue;
+boolean dht11_flag = true;
+char msg[15];
 
 // Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
@@ -23,6 +27,8 @@ char server[] = "10.10.141.43";
 
 // Initialize the Ethernet client object
 WiFiEspClient client;
+
+/************** setup() *******************/
 
 void setup()
 {
@@ -56,14 +62,51 @@ void setup()
   Serial.println();
   Serial.println("Starting connection to server...");
   // if you get a connection, report back via serial
-  if (client.connect(server, 5050)) {
+  if (client.connect(server, 5000)) {
     Serial.println("Connected to server");
   }
+  MsTimer2::set(1000, test);
+  MsTimer2::start();
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(FLAME_PIN, INPUT);
+  pinMode(CDS_PIN, INPUT);
 }
 void loop()
 {
   // if there are incoming bytes available
   // from the server, read them and print them
+
+  cdsValue = analogRead(CDS_PIN)-250;
+  flameValue = digitalRead(FLAME_PIN);
+  digitalWrite(LED_PIN, HIGH);
+
+  if(flameValue){
+    digitalWrite(LED_PIN, HIGH);
+    //flame_state = 1;
+  }
+  else{
+    digitalWrite(LED_PIN, LOW);
+    //flame_state = 0;
+  }
+  
+  if(dht11_flag){
+    char temp, humi;
+    DHT.read(DHT11_PIN);
+    temp = DHT.temperature;
+    humi = DHT.humidity;
+    
+    sprintf(msg, "%d:%d:%d:%dL", humi, temp, flameValue, cdsValue);
+    Serial.println(msg);
+    client.print(msg);
+  }
+  dht11_flag = false;
+
+
+
+
+  
+  /*
   char sensing[10] = {0};
   int index = 0;
   client.print("hello");
@@ -73,6 +116,7 @@ void loop()
   sensing[index] = '\0';
   Serial.println(sensing);
   delay(100);
+  */
   // if the server's disconnected, stop the client
   if (!client.connected()) {
     Serial.println();
@@ -84,6 +128,9 @@ void loop()
   }
 }
 
+void test(){
+  dht11_flag = !dht11_flag;
+}
 
 void printWifiStatus()
 {
